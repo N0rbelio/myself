0b2ef3a6a38f4bccac9a547fe0b3601c027fdac0
N0rbelio <n0bledeath.117@gmail.com>
2024-05-14 16:18:12 +0100

    big changes

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
   // Function to check scroll position and apply appropriate class to nav
-  function checkScrollPosition() {
+/*   function checkScrollPosition() {
     var nav = document.getElementById('navbar');
     if (window.pageYOffset > 20) {
       nav.classList.add('hidden');
@@ -8,10 +8,30 @@
       nav.classList.remove('hidden');
       nav.classList.add('full');
     }
-  }
+  } */
 
   // Check scroll position when page loads
-  window.addEventListener('load', checkScrollPosition);
+/*   window.addEventListener('load', checkScrollPosition); */
 
   // Check scroll position on scroll event
-  window.addEventListener("scroll", checkScrollPosition);
+/*   window.addEventListener("scroll", checkScrollPosition); */
+
+
+
+  /* nav bar scripting */
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let scrollTopBtn = document.querySelector("#scroll_top"); 
+  closeBtn.addEventListener("click", ()=> { /* close event  */ 
+      sidebar.classList.toggle("open");   
+      menuBtnChange();/* alterar btn */
+      });
+
+  function menuBtnChange() {
+      if(sidebar.classList.contains("open")) {         /* validar se esta aberto ou n  */
+          closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      }
+      else {  
+          closeBtn.classList.replace("bx-menu-alt-right","bx-menu"); 
+      }
+  }
